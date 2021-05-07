a696c052403d21d99268329a76a6abafd79922d8
jango-blockchained <info@cryptolinx.de>
2021-05-07 19:25:38 +0200

    .

--- plotter/run.sh
+++ plotter/run.sh
@@ -3,6 +3,6 @@
 for (( c=1; c<=$1; c++ ))
 do
   docker run -d --name plotter$c --network none -v /mnt/scratch/plotter$c:/tmp -v /mnt/storage/plots:/plots clx/chia-plotter:latest
-  echo STARTED PLOTTER $c ... sleep
-  sleep 1m
+  echo 🌱 STARTED PLOTTER $c of $1 ... sleep for 1h
+  sleep 1h
 done

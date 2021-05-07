1e1ce88157f2612a7265ddca4d014f66ee327c90
jango-blockchained <info@cryptolinx.de>
2021-05-07 18:10:08 +0200

    added mounting

--- setup.sh
+++ setup.sh
@@ -10,3 +10,18 @@ apt install -y make zip unzip docker.io docker-compose cifs-utils
 echo MOVE KEYFILE
 cp keyfile farmer/keyfile
 mv keyfile plotter/keyfile
+
+echo MOUNT SCRATCH /tmp - /mnt/scratch/plotter#
+mkdir /mnt/scratch
+chmod +x /mnt/storage
+mount -o discard,defaults /dev/disk/by-id/scsi-0HC_Volume_11049661 /mnt/scratch
+echo "/dev/disk/by-id/scsi-0HC_Volume_11049661 /mnt/scratch ext4 discard,nofail,defaults 0 0" >> /etc/fstab
+mount -a
+
+echo MOUNT STORAGE /plots - /mnt/storage
+mkdir /mnt/storage
+chmod +x /mnt/storage
+mount -t cifs //u264756.your-storagebox.de/backup /mnt/storage -o sec=ntlmv2,username=u264756,password=8xdgBFtd02w2ETcr
+echo "//u264756.your-storagebox.de/backup /mnt/storage cifs auto,users,credentials=~/.smbcredentials,uid=1000,gid=1000,file_mode=0644,dir_mode=0755  0 0
+" >> /etc/fstab
+mount -a

d3b2dc18249f768f444e20d3cc739ff153678ad4
jango-blockchained <info@cryptolinx.de>
2021-05-07 19:06:50 +0200

    added 🌱 to stdout

--- setup.sh
+++ setup.sh
@@ -1,27 +1,27 @@
-echo START SETUP
-echo SET PERMISSION
+echo 🌱 START SETUP
+echo 🌱 SET PERMISSION
 chmod +x *.sh
 chmod +x farmer/*.sh
 chmod +x plotter/*.sh
 
-echo INSTALL DEPENDENCIES
+echo 🌱 INSTALL DEPENDENCIES
 apt install -y make zip unzip docker.io docker-compose cifs-utils
 
-echo MOVE KEYFILE
+echo 🌱 MOVE KEYFILE
 cp keyfile farmer/keyfile
 mv keyfile plotter/keyfile
 
-echo MOUNT SCRATCH /tmp - /mnt/scratch/plotter#
+echo 🌱 MOUNT SCRATCH /tmp - /mnt/scratch/plotter#
 mkdir /mnt/scratch
-chmod +x /mnt/storage
+chmod +x /mnt/scratch
 mount -o discard,defaults /dev/disk/by-id/scsi-0HC_Volume_11049661 /mnt/scratch
-echo "/dev/disk/by-id/scsi-0HC_Volume_11049661 /mnt/scratch ext4 discard,nofail,defaults 0 0" >> /etc/fstab
+echo 🌱 "/dev/disk/by-id/scsi-0HC_Volume_11049661 /mnt/scratch ext4 discard,nofail,defaults 0 0" >> /etc/fstab
 mount -a
 
-echo MOUNT STORAGE /plots - /mnt/storage
+echo 🌱 MOUNT STORAGE /plots - /mnt/storage
 mkdir /mnt/storage
 chmod +x /mnt/storage
 mount -t cifs //u264756.your-storagebox.de/backup /mnt/storage -o sec=ntlmv2,username=u264756,password=8xdgBFtd02w2ETcr
-echo "//u264756.your-storagebox.de/backup /mnt/storage cifs auto,users,credentials=~/.smbcredentials,uid=1000,gid=1000,file_mode=0644,dir_mode=0755  0 0
+echo 🌱 "//u264756.your-storagebox.de/backup /mnt/storage cifs auto,users,credentials=~/.smbcredentials,uid=1000,gid=1000,file_mode=0644,dir_mode=0755  0 0
 " >> /etc/fstab
 mount -a

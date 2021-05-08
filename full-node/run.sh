3ca5137ae3092ba1ec92a95580d9fce6a1b82c77
jango-blockchained <info@cryptolinx.de>
2021-05-08 20:37:24 +0200

    .

--- full-node/run.sh
+++ full-node/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # docker run -it -p 8444:8444 --name full_node -v chia-db:/root/.chia -v /mnt/storage:/plots:ro -e keys="./keyfile" clx/chia-full_node
-docker run --name full_node -d --dns=1.1.1.1 -v /mnt/scratch/full-node:/tmp -v /mnt/storage:/plots:ro -e keys="./keyfile" ghcr.io/chia-network/chia:latest
+docker run --name full_node -d --dns=1.1.1.1 -v /mnt/scratch/full-node:/tmp -v /mnt/storage:/plots:ro ghcr.io/chia-network/chia:latest
 

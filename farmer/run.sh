d7c30017910f469a7d46ffa060a13a0fb06aae54
jango-blockchained <info@cryptolinx.de>
2021-05-08 15:36:24 +0200

    .

--- farmer/run.sh
+++ farmer/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -p 8444:8444 --name farmer${FID} -v chia-db:/root/.chia -v /mnt/storage:/plots:ro clx/chia-farmer
+docker run -it -p 8444:8444 --name farmer -v chia-db:/root/.chia -v /mnt/storage:/plots:ro clx/chia-farmer

72f7d3d7d2645042142ce815cba0efb4515f08ad
jango-blockchained <info@cryptolinx.de>
2021-05-08 15:13:25 +0200

    .

--- plotter/run-single.sh
+++ plotter/run-single.sh
@@ -1,1 +1,7 @@
-docker run -d --name plotter${NR} --network none -v /mnt/scratch/plotter${NR}:/tmp -v /mnt/storage/plots:/plots clx/chia-plotter:latest
+#!/bin/bash
+
+if [[ ! -z $1 ]]; then
+    docker run -d --name plotter$1 --network host --dns=1.1.1.1 -v /mnt/scratch/plotter$1:/tmp -v /mnt/storage:/plots clx/chia-plotter:latest
+else
+    echo 
+fi
--- plotter/run.sh
+++ plotter/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for (( c=1; c<=$1; c++ ))
+for (( c=1; c<=${PA}; c++ ))
 do
-  docker run -d --name plotter$c --network none -v /mnt/scratch/plotter$c:/tmp -v /mnt/storage/plots:/plots clx/chia-plotter:latest
-  echo 🌱 STARTED PLOTTER $c of $1 ... sleep for 1h
+  docker run -d --name plotter$c --network host --dns=1.1.1.1 -v /mnt/scratch/plotter$c:/tmp -v /mnt/storage:/plots clx/chia-plotter:latest
+  echo 🌱 STARTED PLOTTER $c of ${PA} ... sleep for 1h
   sleep 1h
 done

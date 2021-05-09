d375a4e4596623fcf3650b94d4eeb0ed8780871a
jango-blockchained <info@inart-designs.de>
2021-05-09 13:39:10 +0200

    .

--- plotter/init.sh
+++ plotter/init.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-while [ ! -f /root/stoprun ]
-do
-  echo 🌱 CLEANING... /tmp
-  rm /tmp/*.tmp
-  echo 🌱 CREATE K32 PLOTS - 4/1/PARA
-  chia plots create -r 2 -t /tmp -d /plots
-done
-rm -f /root/stoprun
-echo 🌱 STOPFILE FOUND, GRACEFULL EXIT AFTER PLOTTING
+if [[ -f /root/plotman]]; then
+  echo 🌱 START PLOTTING VIA PLOTMAN
+  plotman plot
+else
+  while [ ! -f /root/stoprun ]
+  do
+    echo 🌱 CLEANING... /tmp
+    rm /tmp/*.tmp
+    echo 🌱 CREATE K32 PLOTS - 4/1/PARA
+    chia plots create -r 2 -t /tmp -d /plots
+  done
+  rm -f /root/stoprun
+  echo 🌱 STOPFILE FOUND, GRACEFULL EXIT AFTER PLOTTING

359a3fb6c7f282b10a136eb1be99bd72aae75fd0
jango-blockchained <info@cryptolinx.de>
2021-05-09 15:50:15 +0200

    .

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 echo 🌱 BUILD IMAGES
 
-docker build -t clx/chia-farmer farmer/ --no-cache
-docker build -t clx/chia-plotter plotter/ --no-cache
+docker build -t clx/chia-farmer farmer/
+docker build -t clx/chia-plotter plotter/
--- farmer/init.sh
+++ farmer/init.sh
@@ -4,13 +4,12 @@ chia init
 chia keys add -f /root/keyfile
 rm /root/keyfile
 sed -i 's/localhost/127.0.0.1/g' ~/.chia/mainnet/config/config.yaml
-# chia plots add -d /plots
+chia plots add -d /plots
 chia start farmer
-# chia plots check
-# while [ 1 ]
-# do
-#   chia show -s
-#   chia wallet show
-#   sleep 1h
-# done
-plotman status
+chia plots check
+while [ 1 ]
+do
+  chia show -s
+  chia wallet show
+  sleep 1h
+done

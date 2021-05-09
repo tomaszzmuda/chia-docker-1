ecdbed5459b4107f383bff927e3a92d334d3e70c
jango-blockchained <info@inart-designs.de>
2021-05-09 13:10:13 +0200

    updates

--- farmer/init.sh
+++ farmer/init.sh
@@ -4,12 +4,13 @@ chia init
 chia keys add -f /root/keyfile
 rm /root/keyfile
 sed -i 's/localhost/127.0.0.1/g' ~/.chia/mainnet/config/config.yaml
-chia plots add -d /plots
+# chia plots add -d /plots
 chia start farmer
-chia plots check
-while [ 1 ]
-do
-  chia show -s
-  chia wallet show
-  sleep 1h
-done
+# chia plots check
+# while [ 1 ]
+# do
+#   chia show -s
+#   chia wallet show
+#   sleep 1h
+# done
+plotman status

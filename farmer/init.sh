ccc3b9a63b84d4859b18bcb2525f7e271b75ead5
jango-blockchained <info@inart-designs.de>
2021-05-11 18:47:45 +0200

    .

--- farmer/init.sh
+++ farmer/init.sh
@@ -4,9 +4,9 @@ chia init
 chia keys add -f /root/keyfile
 rm /root/keyfile
 sed -i 's/localhost/127.0.0.1/g' ~/.chia/mainnet/config/config.yaml
-chia plots add -d /plots
+chia plots add -d /dst
 chia start farmer
-chia plots check
+chia plots status
 while [ 1 ]
 do
   chia show -s
--- farmer/run.sh
+++ farmer/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -p 8444:8444 --name farmer --dns=1.1.1.1 -v chia-db:/root/.chia -v /mnt/storage:/plots:ro clx/chia-farmer
+docker run -it -p 8444:8444 --name farmer --dns=1.1.1.1 -v chia-db:/root/.chia -v /mnt/storage:/storage:ro clx/chia-farmer

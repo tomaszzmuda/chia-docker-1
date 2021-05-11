38a01fa56d909f5b94dd473ad6be09fcfe54d5cb
jango-blockchained <info@inart-designs.de>
2021-05-11 18:24:28 +0200

    performance opt.

--- plotter/init.sh
+++ plotter/init.sh
@@ -3,9 +3,9 @@
 while [ ! -f /root/stoprun ]
 do
   echo 🌱 CLEANING... /tmp
-  # rm -v /tmp/*.tmp
+  rm -v /tmp/*.tmp
   echo 🌱 CREATE K32 PLOTS - 4/1/PARA
-  chia plots create -r 2 -t /tmp -d /plots
+  chia plots create -r 2 -t /tmp -2 /tmp2 -d /dst
 done
 rm -f /root/stoprun
 echo 🌱 STOPFILE FOUND, GRACEFULL EXIT AFTER PLOTTING
--- plotter/run.sh
+++ plotter/run.sh
@@ -2,7 +2,7 @@
 
 for (( c=1; c<=${PA}; c++ ))
 do
-  docker run -d --name plotter$c --dns=1.1.1.1 -v /mnt/scratch/plotter$c:/tmp -v /mnt/storage:/plots clx/chia-plotter:latest
-  echo 🌱 STARTED PLOTTER $c of ${PA} ... sleep for 1h
+  docker run -d --name plotter$c --dns=1.1.1.1 -v /mnt/scratch/tmp_$c:/tmp  -v /mnt/scratch/tmp2_$c:/tmp2  -v /mnt/scratch/dst_$c:/dst -v /mnt/storage:/storage clx/chia-plotter:latest
+  echo 🌱 PLOTTER STARTED $c of ${PA} ... sleep for 1h
   sleep 1h
 done
